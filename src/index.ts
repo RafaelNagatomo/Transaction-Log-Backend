1ecef238a5a84aace2fcfb07d077b8df6ed054db
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2025-01-25 00:20:40 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from '~/infrastructure/config/database'
 import authRoutes from '~/presentation/routes/authRoutes'
+import cors from 'cors'
 
 dotenv.config()
 const PORT = process.env.PORT || 4000
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/auth', authRoutes)
@@ -14,5 +16,5 @@ app.use('/auth', authRoutes)
 connectDB()
 
 app.listen(PORT, () => {
-  console.log('Server is running on port 4000...')
+  console.log(`Server is running on port ${PORT}...`)
 })
